7639a45755d5bc1a79789ee995d5d2843f64e3c0
sarahmorgantordeur-lab <sarahmorgantordeur@gmail.com>
2026-02-02 10:39:21 +0100

    app-module.ts completion + product-demo.module.ts

--- src/app/features/product-demo/product-demo.module.ts
+++ src/app/features/product-demo/product-demo.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductCardComponent } from './components/product-card/ProductCardComponent';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    ProductCardComponent,
     ProductListComponent
   ],
   exports: [
-    ProductListComponent
+    ProductListComponent,
+    ProductCardComponent
   ]
 })
-export class UserDemoModule { }
+export class ProductDemoModule { }
